55c9d848e7854ef36f381911e6eda2bf188ac266
zhufengnodejs <zhufengnodejs@126.com>
2016-08-24 15:57:10 +0800

    中间件

--- 7.es6/9.obj.js
+++ 7.es6/9.obj.js
@@ -2,7 +2,7 @@ let name = 'zfpx';
 let age = 8;
 
 //console.log(this === module.exports);
-let getName = function(){
+let getName = function () {
     console.log(this.name);
 }
 let person = {
@@ -14,43 +14,41 @@ let person = {
 
 //console.log(NaN == NaN);
 //console.log(Object.is(NaN,NaN));
-var nameObj = {name:'zfpx'};
-var ageObj = {age:8};
+var nameObj = {name: 'zfpx'};
+var ageObj = {age: 8};
 //var obj = {};
-Object.assign = function(target,...sources){
-    function copy(target,source){
-        var sourceType = Object.prototype.toString.call(source);
-        if(sourceType == '[object Object]'){
-            for(let attr in source){
-                var str = Object.prototype.toString.call(source[attr]);
-                //如果是一个对象
-                if(str == '[object Object]'){
-                    var newObj = {};//构建一个空对象
-                    copy(newObj,source[attr]);
-                    target[attr] = newObj;
-                }else if(str == '[object Array]'){//如果属性是一个数组
-                    var newArr = [];
-                    copy(newArr,source[attr]);
-                    target[attr] = newArr;
-                }else{
-                    //当属性是值的时候直接赋值
-                    target[attr] =source[attr];
+Object.assign = function (target, ...sources) {
+    function copy(target, source) {
+        for (let attr in source) {
+            var str = Object.prototype.toString.call(source[attr]);
+            //如果是一个对象
+            if (str == '[object Object]') {
+                var newObj = {};//构建一个空对象
+                for(var key in source[attr]){
+                    newObj[key] = source[attr][key];
                 }
-            }
-        }else if(sourceType == '[object Array]'){
-            for(var i=0;i,source.length;i++){
-                target[i] = source[i];
+                target[attr] = newObj;
+            } else if (str == '[object Array]') {//如果属性是一个数组
+                var newArr = [];
+                for(var i=0;i<source[attr].length;i++){
+                    newArr[i] = source[attr][i];
+                }
+                target[attr] = newArr;
+            } else {
+                //当属性是值的时候直接赋值
+                target[attr] = source[attr];
             }
         }
 
     }
-    sources.forEach(source=>{
-        copy(target,source);
+
+    sources.forEach(source=> {
+        copy(target, source);
     });
 }
 var obj = {};
-var source = {hobbies:['smoking','drinking']}
-Object.assign(obj,source);
+var source = {hobbies: ['smoking', 'drinking']}
+Object.assign(obj, source);
 console.log(obj);
 console.log(source);
 obj.hobbies.push('hair');
