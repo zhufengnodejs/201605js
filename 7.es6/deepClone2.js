d65582007d43072c303b4712909f75c871e3864d
zhufengnodejs <zhufengnodejs@126.com>
2016-08-25 10:36:40 +0800

    jsx语法

---
+++ 7.es6/deepClone2.js
@@ -0,0 +1,15 @@
+var cloneObj = function(obj){
+    var str, newobj = obj.constructor === Array ? [] : {};
+    if(typeof obj !== 'object'){
+        return;
+    } else if(window.JSON){
+        str = JSON.stringify(obj), //系列化对象
+            newobj = JSON.parse(str); //还原
+    } else {
+        for(var i in obj){
+            newobj[i] = typeof obj[i] === 'object' ?
+                cloneObj(obj[i]) : obj[i];
+        }
+    }
+    return newobj;
+};
--- 8.react/2.jsx.js
+++ 8.react/2.jsx.js
@@ -3,12 +3,15 @@
  */
 //声明一个普通的JS的数组
 var names = ['张龙','赵虎','王朝'];
+var style = {color:'red'};
 ReactDOM.render(
     //有且只能一个顶级元素
     <div>
         { //由是遇到{}以JS语法来进行解析
-            names.map(function(item){
-                return <div>{item}</div>
+            //如果是迭代一个数组的话，每个子元素都需要有一个独一无二的key属性
+            names.map(function(item,index){
+                //如果要指定样式的话，需要让属性指向一个JS对象
+                return <div className="green" style={style} key={index}>{item}</div>
             })
         }
     </div>,
