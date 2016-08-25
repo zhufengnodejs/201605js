f048cb1ddaedc243c9b1c0d840898300ec176e67
zhufengnodejs <zhufengnodejs@126.com>
2016-08-25 17:50:55 +0800

    实现留言功能

--- 8.react/13.board.js
+++ 8.react/13.board.js
@@ -1,11 +1,12 @@
 var Board = React.createClass({
     getInitialState(){
-      return {messages:[]};
+      return {messages:this.props.messages};
     },
     handleClick(event){//处理点击事件
         event.preventDefault();
         var value = this.refs.txtMsg.value;
         this.state.messages.push(value);
+        localStorage.setItem('messages',JSON.stringify(this.state.messages));
         //修改状态一定要通过setState.因为setState是一个异步方法，
         //而且会在状态修改后尝试尝试视图 ['a','b']
         this.setState({messages:this.state.messages},function(){
@@ -40,4 +41,9 @@ var Board = React.createClass({
         )
     }
 });
-ReactDOM.render(<Board/>,document.querySelector('#app'));
+//localStorage是一个window全局变量。
+// setItem(key,value) getItem(key)
+var messages = localStorage.getItem('messages');
+messages = messages?JSON.parse(messages):[];
+
+ReactDOM.render(<Board messages={messages}/>,document.querySelector('#app'));
