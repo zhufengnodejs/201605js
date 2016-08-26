1aabe0b3f9464e150eeaddf9bb3429c4a4061a93
zhufengnodejs <zhufengnodejs@126.com>
2016-08-26 22:04:25 +0800

    实现留言功能

--- 9.es6/6.co.js
+++ 9.es6/6.co.js
@@ -4,24 +4,27 @@ function co(gene){
   function next(data){
      var result = it.next(data);
       console.log(result);
-     while(!result.done){
+     if(!result.done){
         result.value.then(next);
      }
   }
   next();
 }
+var fs =require('fs');
 co(function* (){
     console.log(1);
     var a = yield new Promise(function(resolve,reject){
+        console.log('开始a');
         setTimeout(function(){
+            console.log('结束a');
             resolve('a');
-        },1000);
+        },100);
     });
     console.log(a);
     var b = yield new Promise(function(resolve,reject){
         setTimeout(function(){
             resolve(a+'-b');
-        },1000);
+        },100);
     });
     console.log(b);
 });
