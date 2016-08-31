0f2971e89c5f760ae132ee542cd33fe58846eecb
zhufengnodejs <zhufengnodejs@126.com>
2016-08-31 12:52:12 +0800

    回调的写法

--- 11.promise/bluebird.js
+++ 11.promise/bluebird.js
@@ -1,15 +1,20 @@
 module.exports = {
+    //拥有一个promisify的属性，接收一个原来的异步函数 readFile
     promisify(fn){
         return function(){
+            //得到传给函数的参数 并转成数组
             var args = Array.from(arguments);
             return new Promise(function(resolve,reject){
-                fn.apply(null,args.concat([function(err,data){
+                //在此调用readFile方法
+                fn.apply(null,args.concat(function(err,data){
                     if(err){
+                        //如果失败会调用失败的回调
                         reject(err);
                     }else{
+                        //如果成功会调用成功的回调
                         resolve(data);
                     }
-                }]));
+                }));
             });
         }
     },
